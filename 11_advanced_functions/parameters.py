c8ee6919cab0d20fa501be4d3edb881c7e6bcfdb
Manasa_018 <237534003+ManasaUmashankar@users.noreply.github.com>
2026-09-08 23:27:14 +0530

    Create parameters.py

---
+++ 11_advanced_functions/parameters.py
@@ -0,0 +1,23 @@
+# Parameters vs Arguments
+
+def introduce(name, age, course):
+    print("Name:", name)
+    print("Age:", age)
+    print("Course:", course)
+
+
+# Positional arguments
+introduce("Manasa", 19, "BCA")
+
+# Keyword arguments
+introduce(name="Manasa", age=19, course="BCA")
+
+----
+Expected output
+
+Name: Manasa
+Age: 19
+Course: BCA
+Name: Manasa
+Age: 19
+Course: BCA
