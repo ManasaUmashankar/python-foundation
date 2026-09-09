885530f2815efddf85e8ef4da45aecce7eb7f31e
Manasa_018 <237534003+ManasaUmashankar@users.noreply.github.com>
2026-09-09 23:37:02 +0530

    Add math_utils.py with add and subtract functions
    Defined basic math utility functions for addition and subtraction.

---
+++ 12_modules_packages/math_utils.py
@@ -0,0 +1,17 @@
+File 1 — math_utils.py
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+------
+
+Expected output
+
+Nothing.
+
+That's completely correct. We only defined the functions.
+
+The actual output will come when main.py imports and uses this module.
