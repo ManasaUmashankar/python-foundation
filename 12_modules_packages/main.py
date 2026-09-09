826a139ba76a9f2f30de9d10b6f8bca805a8b3ba
Manasa_018 <237534003+ManasaUmashankar@users.noreply.github.com>
2026-09-09 23:38:23 +0530

    Create main.py to demonstrate math_utils usage
    Implement main.py to utilize math_utils functions for addition and subtraction.

---
+++ 12_modules_packages/main.py
@@ -0,0 +1,29 @@
+import math_utils
+
+result1 = math_utils.add(10, 5)
+result2 = math_utils.subtract(10, 5)
+
+print("Addition:", result1)
+print("Subtraction:", result2)
+
+
+-------
+Expected output
+Addition: 15
+Subtraction: 5
+🔍 Remember this
+import math_utils
+
+means:
+"Make the code inside math_utils.py available here."
+
+And:
+math_utils.add(10, 5)
+
+means:
+"Use the add() function from math_utils."
+Run main.py, not math_utils.py.
+
+Once you get:
+Addition: 15
+Subtraction: 5
