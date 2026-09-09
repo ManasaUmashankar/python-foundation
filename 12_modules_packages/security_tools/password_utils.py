400b60534326cd4ea0daf724c2a2b01f2fbf37d2
Manasa_018 <237534003+ManasaUmashankar@users.noreply.github.com>
2026-09-09 23:50:28 +0530

    Add password length check function
    Define a function to check password length.

---
+++ 12_modules_packages/security_tools/password_utils.py
@@ -0,0 +1,10 @@
+ef check_password_length(password):
+    """Check whether a password has at least 8 characters."""
+
+    return len(password) >= 8
+
+
+-----------
+Expected output
+Nothing.
+{We're defining the function, not calling it yet,We'll use this function from main.py after we create the package modules}
