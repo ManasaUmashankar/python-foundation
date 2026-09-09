60a584d8e0491eb10e8b4f96a10e3d6098c72717
Manasa_018 <237534003+ManasaUmashankar@users.noreply.github.com>
2026-09-10 00:03:31 +0530

    Clean up comments in multiple_returns.py
    Removed comments explaining multiple return values.

--- 11_advanced_functions/multiple_returns.py
+++ 11_advanced_functions/multiple_returns.py
@@ -21,19 +21,3 @@ print("Age:", age)
 Name: Manasa
 Role: SOC Analyst
 Age: 19
-🧠 What's happening here?
-
-This: return name, role, age
-       {returns multiple values as a tuple.}
-
-Then:
-
-name, role, age = user_details()
-   {unpacks those values into separate variables.}
-
-its just like:
-("Manasa", "SOC Analyst", 19)
-       ↓
- name      role        age
-
-it has unpacked the 3 values into 3 separate variables
