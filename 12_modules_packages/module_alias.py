3a762ce7e97aad667e50c51492d147b399dc3fde
Manasa_018 <237534003+ManasaUmashankar@users.noreply.github.com>
2026-09-09 23:41:36 +0530

    Create module_alias.py

---
+++ 12_modules_packages/module_alias.py
@@ -0,0 +1,12 @@
+import math_utils as mu
+result1 = mu.add(20, 10)
+result2 = mu.subtract(20, 10)
+
+
+print("Addition:", result1)
+print("Subtraction:", result2)
+
+----
+Expected Output
+Addition: 30
+Subtraction: 10
